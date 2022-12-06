ba124b8a3946146ee31500bc7f44c2a3c5e0728c
Katharina Hämmerl <haemmerl@cis.lmu.de>
2022-12-06 17:25:27 +0100

    understanding the plots.py

--- from-alina/code/analysis_code/plots.py
+++ from-alina/code/analysis_code/plots.py
@@ -3,6 +3,7 @@ import torch
 import matplotlib.pyplot as plt
 import seaborn as sns
 import os
+import numpy as np
 
 parser = argparse.ArgumentParser(description='Generate embedding visualizations.')
 parser.add_argument('--model', type=str, help="name of the model (xlm-r, x2s-cca or x2s-mse)")
@@ -33,63 +34,67 @@ lang_dict_3_2 = dict((v, k) for k, v in lang_dict.items())
 
 # produce heatmaps of sentence embeddings per language (not featured in thesis)
 def heatmaps(tgt_embs, eng_embs, tgt_lang):
-    dir_tgt = f'plots_layer_{args.layer}/{tgt_lang}/{args.model}/{tgt_lang}.png'
-    dir_eng = f'plots_layer_{args.layer}/{tgt_lang}/{args.model}/eng.png'
-    os.makedirs(os.path.dirname(dir_tgt), exist_ok=True)
-    os.makedirs(os.path.dirname(dir_eng), exist_ok=True)
+    fig_dir = f'../plots/{args.model}/{args.layer}/{tgt_lang}/'
+    os.makedirs(os.path.dirname(fig_dir), exist_ok=True)
 
-    plt.figure(figsize=(8, 2))
+    fig = plt.figure(figsize=(8, 2))
     ax = sns.heatmap(tgt_embs, cmap="YlGnBu", yticklabels=False, xticklabels=100)
     plt.gca().invert_yaxis()
     plt.title(f"{tgt_lang} representations")
     plt.ylabel("# sentence")
     plt.xlabel("dimension")
-    plt.savefig(dir_tgt, dpi=300, bbox_inches="tight")
+    plt.savefig(f'{fig_dir}/heatmap_{tgt_lang}.png', dpi=300, bbox_inches="tight")
     # plt.show()
+    plt.close(fig)
 
-    plt.figure(figsize=(8, 2))
+    fig = plt.figure(figsize=(8, 2))
     ex = sns.heatmap(eng_embs, cmap="YlGnBu", yticklabels=False, xticklabels=100)
     plt.gca().invert_yaxis()
     plt.title("eng representations")
     plt.ylabel("# sentence")
     plt.xlabel("dimension")
-    plt.savefig(dir_eng, dpi=300, bbox_inches="tight")
+    plt.savefig(f'{fig_dir}/heatmap_eng.png', dpi=300, bbox_inches="tight")
     # plt.show()
+    plt.close(fig)
     print(f"Finished {tgt_lang} heatmaps.")
 
 
 # produce visualizations of language level mean embeddings (for target and english)
+# not in thesis i think?
 def mean_vec(tgt_embs, eng_embs, tgt_lang):
-    dir_tgt = f'plots_layer_{args.layer}/{tgt_lang}/{args.model}/mean_{tgt_lang}.png'
-    dir_eng = f'plots_layer_{args.layer}/{tgt_lang}/{args.model}/mean_eng.png'
-    os.makedirs(os.path.dirname(dir_tgt), exist_ok=True)
-    os.makedirs(os.path.dirname(dir_eng), exist_ok=True)
+    fig_dir = f'../plots/{args.model}/{args.layer}/{tgt_lang}/'
+    os.makedirs(os.path.dirname(fig_dir), exist_ok=True)
 
     tgt_mean_vec = torch.mean(tgt_embs, axis=0)
     eng_mean_vec = torch.mean(eng_embs, axis=0)
 
-    plt.figure(figsize=(4, 2))
+    fig = plt.figure(figsize=(4, 2))
     plt.plot(tgt_mean_vec)
     plt.title(f"{tgt_lang} mean representation")
     plt.xlabel("dimension")
-    plt.savefig(dir_tgt, dpi=300, bbox_inches="tight")
+    plt.savefig(f'{fig_dir}/mean_{tgt_lang}.png', dpi=300, bbox_inches="tight")
     # plt.show()
+    plt.close(fig)
 
-    plt.figure(figsize=(4, 2))
+    fig = plt.figure(figsize=(4, 2))
     plt.plot(eng_mean_vec)
     plt.title("eng mean representation")
     plt.xlabel("dimension")
-    plt.savefig(dir_eng, dpi=300, bbox_inches="tight")
+    plt.savefig(f'{fig_dir}/mean_eng.png', dpi=300, bbox_inches="tight")
     # plt.show()
+    plt.close(fig)
     print(f"Finished {tgt_lang} mean vecs.")
 
 
 # Analyze average english embedding
+# ref fig 5.4 in thesis
 def mean_english():
+    fig_dir = f'../plots/{args.model}/{args.layer}/'
+    os.makedirs(os.path.dirname(fig_dir), exist_ok=True)
     mean_sum_vec = None
     for lang in langs:
         print(f"Processing parallel English of: {lang}")
-        eng_emb = torch.load(f'embs_layer_{args.layer}/{args.model}/{lang}/eng.pt')
+        eng_emb = torch.load(f'../embs/{args.model}/{args.layer}/{lang}/eng.pt')
         if mean_sum_vec is None:
             mean_sum_vec = torch.mean(eng_emb, axis=0)
         else:
@@ -98,6 +103,8 @@ def mean_english():
     mean_vec = torch.divide(mean_sum_vec, len(langs))
     mean = torch.mean(mean_vec)
     std_dev = torch.std(mean_vec)
+    x = np.arange(768)
+    clrs = sns.color_palette("pastel", 8)
 
     counter = 0
     outliers = []
@@ -112,17 +119,18 @@ def mean_english():
 
     plt.figure(figsize=(6, 1.5))
     plt.plot(mean_vec)
+    plt.fill_between(x, mean - std_dev * 3, mean + std_dev * 3, alpha=0.5, facecolor=clrs[7])
     plt.title(f"average english embedding")
     plt.xlabel("dim")
-    plt.savefig("avg_eng_embedding.png", dpi=300, bbox_inches="tight")
+    plt.savefig(f'{fig_dir}/avg_eng_embedding.png', dpi=300, bbox_inches="tight")
 
 
 # plot sentence level outliers (single values in an outlier dimension)
 # can plot either only target lang values or together with english aligned ones
 # (comment/uncomment parts accordingly)
 def plot_outliers(tgt_embs, eng_embs, tgt_lang):
-    dir_tgt = f'dim_plots_layer_{args.layer}/{tgt_lang}/{args.model}/{tgt_lang}_{args.dim}.png'
-    dir_tgt_en = f'dim_plots_layer_{args.layer}/{tgt_lang}/{args.model}/eng_{tgt_lang}_{args.dim}.png'
+    dir_tgt = f'../plots/{args.model}/{args.layer}/{tgt_lang}/{tgt_lang}_dim_{args.dim}.png'
+    dir_tgt_en = f'../plots/{args.model}/{args.layer}/{tgt_lang}/eng_{tgt_lang}_dim_{args.dim}.png'
     os.makedirs(os.path.dirname(dir_tgt), exist_ok=True)
     os.makedirs(os.path.dirname(dir_tgt_en), exist_ok=True)
 
@@ -140,6 +148,7 @@ def plot_outliers(tgt_embs, eng_embs, tgt_lang):
     plt.title(f"{lang_dict_3_2[tgt_lang]}-en dimension {args.dim}")
     plt.xlabel("#sentence")
     plt.savefig(dir_tgt_en, dpi=300, bbox_inches="tight")
+    plt.close()
 
 
 def main():
@@ -147,30 +156,30 @@ def main():
         print("Generating heatmaps.")
         for lang in langs:
             print(f"Current language: {lang}")
-            target_emb = torch.load(f'embs_layer_{args.layer}/{args.model}/{lang}/{lang}.pt')
-            eng_emb = torch.load(f'embs_layer_{args.layer}/{args.model}/{lang}/eng.pt')
+            target_emb = torch.load(f'../embs/{args.model}/{args.layer}/{lang}/{lang}.pt')
+            eng_emb = torch.load(f'../embs/{args.model}/{args.layer}/{lang}/eng.pt')
             heatmaps(target_emb, eng_emb, lang)
 
     elif args.job == "means":
         print("Generating mean representations.")
         for lang in langs:
             print(f"Current language: {lang}")
-            target_emb = torch.load(f'embs_layer_{args.layer}/{args.model}/{lang}/{lang}.pt')
-            eng_emb = torch.load(f'embs_layer_{args.layer}/{args.model}/{lang}/eng.pt')
+            target_emb = torch.load(f'../embs/{args.model}/{args.layer}/{lang}/{lang}.pt')
+            eng_emb = torch.load(f'../embs/{args.model}/{args.layer}/{lang}/eng.pt')
             mean_vec(target_emb, eng_emb, lang)
 
     elif args.job == "outlier":
         print("Generating outlier visualization.")
         if args.language != "":
             lang = lang_dict[args.language]
-            target_emb = torch.load(f'embs_layer_{args.layer}/{args.model}/{lang}/{lang}.pt')
-            eng_emb = torch.load(f'embs_layer_{args.layer}/{args.model}/{lang}/eng.pt')
+            target_emb = torch.load(f'../embs/{args.model}/{args.layer}/{lang}/{lang}.pt')
+            eng_emb = torch.load(f'../embs/{args.model}/{args.layer}/{lang}/eng.pt')
             plot_outliers(target_emb, eng_emb, lang)
         else:
             for lang in langs:
                 print(f"Current language: {lang}")
-                target_emb = torch.load(f'embs_layer_{args.layer}/{args.model}/{lang}/{lang}.pt')
-                eng_emb = torch.load(f'embs_layer_{args.layer}/{args.model}/{lang}/eng.pt')
+                target_emb = torch.load(f'../embs/{args.model}/{args.layer}/{lang}/{lang}.pt')
+                eng_emb = torch.load(f'../embs/{args.model}/{args.layer}/{lang}/eng.pt')
                 plot_outliers(target_emb, eng_emb, lang)
 
     elif args.job == "mean_english":
