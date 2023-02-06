7c1099567faf9e386bf6a85e05ed29550a617108
Katharina Hämmerl <k.haemmerl@campus.lmu.de>
2023-02-06 14:37:49 +0100

    add info & code for downloading and extracting data

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ You can use the code to reproduce our results, or adapt it for further experimen
 # Installation
 
 Using a new virtual environment, you can install libraries from the requirements file.
-Check what versions of torch/CUDA and faiss you need, and run:
+Check which versions of torch/CUDA and faiss you need, and run:
 
 ```bash
 pip install -r src/requirements.txt
@@ -19,13 +19,38 @@ For Tatoeba and STS, task scores are calculated as well.
 tSNE visualisations are commented out from ``main.py`` just because they take a long time.
 
 You can either call ``main.py`` directly, or run experiments individually.
-BUCC2018 is not included in the ``main.py``; you can run this task on different models and with zeroed-out dimensions using ``run_bucc2018.py``. 
+BUCC2018 is not included in the ``main.py``; you can run this task on different models and optionally with zeroed-out dimensions using ``run_bucc2018.py``. 
 Check the command line help for how to run it.
 
 Similarly, extracting sentence embeddings, outlier and anisotropy analysis, and plots can all be run individually using the respective command line interfaces.
 
 ## For Figure 1
 
+
+
+
+# Obtaining the Datasets Used
+
+You can obtain the relevant data as follows:
+
+## Tatoeba
+
+You can download the original data from https://github.com/facebookresearch/LASER/archive/master.zip
+We include code in ``extract_sent_embeddings.py`` to download and extract this data to the expected locations.
+
+## BUCC2018
+
+You can download this data from https://comparable.limsi.fr/bucc2018
+We include code in ``extract_sent_embeddings.py`` to download and extract this data to the expected locations.
+
+## Multilingual STS
+
+You can download this data from http://alt.qcri.org/semeval2017/task1/data/uploads/sts2017.eval.v1.1.zip and http://alt.qcri.org/semeval2017/task1/data/uploads/sts2017.gs.zip
+We include code in ``extract_sent_embeddings.py`` to download and extract this data to the expected location.
+
+## Wiki data
+
+Rajaee and Pilehvar (2022) provide this data in their repository: https://github.com/Sara-Rajaee/Multilingual-Isotropy/tree/main/data
 
 
 # License
--- src/extract_sent_embeddings.py
+++ src/extract_sent_embeddings.py
@@ -1,6 +1,8 @@
 """ turning the colab file into a script """
 import argparse
 import os
+import shutil
+import tarfile
 import zipfile
 
 import pandas as pd
@@ -80,6 +82,39 @@ def sentences_from_two_cols(path):
     return col1, col2
 
 
+def download_bucc(langs):
+    os.makedirs('../data/bucc2018/')
+    for lg in langs:
+        wget.download(f'https://comparable.limsi.fr/bucc2018/bucc2018-{lg}-en.training-gold.tar.bz2',
+                      '../data/bucc2018')
+        with tarfile.open(f'../data/bucc2018/bucc2018-{lg}-en.training-gold.tar.bz2', 'r:bz2') as tar:
+            tar.extractall('../data/')
+
+        os.rename(f'../data/bucc2018/{lg}-en/{lg}-en.training.{lg}', f'../data/bucc2018/{lg}-en.test.{lg}')
+        os.rename(f'../data/bucc2018/{lg}-en/{lg}-en.training.en', f'../data/bucc2018/{lg}-en.test.en')
+        os.rename(f'../data/bucc2018/{lg}-en/{lg}-en.training.gold', f'../data/bucc2018/{lg}-en.test.gold')
+
+        os.remove(f'../data/bucc2018/bucc2018-{lg}-en.training-gold.tar.bz2')
+
+        wget.download(f'https://comparable.limsi.fr/bucc2018/bucc2018-{lg}-en.sample-gold.tar.bz2',
+                      '../data/bucc2018')
+        with tarfile.open(f'../data/bucc2018/bucc2018-{lg}-en.sample-gold.tar.bz2', 'r:bz2') as tar:
+            tar.extractall('../data/')
+
+        os.rename(f'../data/bucc2018/{lg}-en/{lg}-en.sample.{lg}', f'../data/bucc2018/{lg}-en.dev.{lg}')
+        os.rename(f'../data/bucc2018/{lg}-en/{lg}-en.sample.en', f'../data/bucc2018/{lg}-en.dev.en')
+        os.rename(f'../data/bucc2018/{lg}-en/{lg}-en.sample.gold', f'../data/bucc2018/{lg}-en.dev.gold')
+        os.remove(f'../data/bucc2018/bucc2018-{lg}-en.sample-gold.tar.bz2')
+        os.rmdir(f'../data/bucc2018/{lg}-en/')
+
+
+def extract_bucc_embeds(args, device, langs, model, tokenizer):
+    if not os.path.exists('../data/bucc2018/'):
+        download_bucc(langs)
+
+    raise NotImplementedError
+
+
 def main(args):
     try:
         int(args.device)
@@ -95,121 +130,164 @@ def main(args):
         langs = []
     elif args.dataset == 'bucc':
         langs = ['de', 'fr', 'ru', 'zh']
-        raise NotImplementedError
     else:
         raise ValueError("unknown dataset argument")
 
     model, tokenizer = load_model(args, device)
 
     if args.dataset == 'tatoeba':
-        for lang in langs:
-            print(f"Currently processing {lang}...")
-            try:
-                os.makedirs(f'../embs/{args.dataset}/{args.model}/{args.layer}/{lang}')
-            except OSError:
-                print(f"embeddings for {lang} already exist, skipping")
-                continue
-            if args.tatoeba_use_task_order:
-                tgt, eng = read_tatoeba_data_task(lang)
-            else:
-                tgt, eng = read_tatoeba_data(lang)
-            tgt_embeddings = get_embeds(tgt, model, tokenizer, args, device)
-            eng_embeddings = get_embeds(eng, model, tokenizer, args, device)
-            torch.save(tgt_embeddings, f'../embs/{args.dataset}/{args.model}/{args.layer}/{lang}/{lang}.pt')
-            torch.save(eng_embeddings, f'../embs/{args.dataset}/{args.model}/{args.layer}/{lang}/eng.pt')
-            if args.save_whitened:
-                tgt_whitened = torch.Tensor(whitening(tgt_embeddings.numpy()))
-                eng_whitened = torch.Tensor(whitening(eng_embeddings.numpy()))
-                torch.save(tgt_whitened, f'../embs/{args.dataset}/{args.model}/{args.layer}/{lang}/{lang}_whitened.pt')
-                torch.save(eng_whitened, f'../embs/{args.dataset}/{args.model}/{args.layer}/{lang}/eng_whitened.pt')
-            if args.save_cbie:
-                n_cluster = max(len(eng) // 300, 1)
-                tgt_cbie = torch.Tensor(cluster_based(
-                    tgt_embeddings.numpy(), n_cluster=n_cluster, n_pc=12, hidden_size=tgt_embeddings.shape[1]))
-                eng_cbie = torch.Tensor(cluster_based(
-                    eng_embeddings.numpy(), n_cluster=n_cluster, n_pc=12, hidden_size=eng_embeddings.shape[1]))
-                torch.save(tgt_cbie, f'../embs/{args.dataset}/{args.model}/{args.layer}/{lang}/{lang}_cbie.pt')
-                torch.save(eng_cbie, f'../embs/{args.dataset}/{args.model}/{args.layer}/{lang}/eng_cbie.pt')
-            print(f"Finished saving embeddings for {lang} in model {args.model}.")
+        extract_tatoeba_embeds(args, device, langs, model, tokenizer)
 
     elif args.dataset == 'wiki':
-        # Loading Wikipedia datasets
-        df_su = pd.read_csv('../data/Wikipedia/Sundanese.csv', sep=',')
-        df_sw = pd.read_csv('../data/Wikipedia/Swahili.csv', sep=',')
-        df_en = pd.read_csv('../data/Wikipedia/English.csv', sep=',')
-        df_es = pd.read_csv('../data/Wikipedia/Spanish.csv', sep=',')
-        df_ar = pd.read_csv('../data/Wikipedia/Arabic.csv', sep=',')
-        df_tr = pd.read_csv('../data/Wikipedia/Turkish.csv', sep=',')
-        dfs = [df_ar, df_en, df_es, df_su, df_sw, df_tr]
-        for lang, df in zip(langs, dfs):
-            print(f"processing {lang}...")
-            try:
-                os.makedirs(f'../embs/{args.dataset}/{args.model}/{args.layer}/{lang}/')
-            except OSError:
-                print(f"embeddings for {lang} already exist, skipping")
-                continue
-            rep = get_embeds(df['Sentence'].tolist(), model, tokenizer, args, device)
-            torch.save(rep, f'../embs/{args.dataset}/{args.model}/{args.layer}/{lang}/{lang}.pt')
-            if args.save_whitened:
-                whitened = torch.Tensor(whitening(rep.numpy()))
-                torch.save(whitened, f'../embs/{args.dataset}/{args.model}/{args.layer}/{lang}/{lang}_whitened.pt')
-            if args.save_cbie:
-                n_cluster = max(rep.shape[0] // 300, 1)
-                cbie = torch.Tensor(
-                    cluster_based(rep.numpy(), n_cluster=n_cluster, n_pc=12, hidden_size=rep.shape[1]))
-                torch.save(cbie, f'../embs/{args.dataset}/{args.model}/{args.layer}/{lang}/{lang}_cbie.pt')
-            print(f"Finished saving embeddings for {lang} in model {args.model}.")
+        extract_wiki_embeds(args, device, langs, model, tokenizer)
 
     elif args.dataset == 'sts':
-        if not os.path.exists('../data/sts'):
-            print("Download and extract the STS data.")
-            os.makedirs('../data/sts/')
-            wget.download("http://alt.qcri.org/semeval2017/task1/data/uploads/sts2017.eval.v1.1.zip", '../data/STS_text.zip')
-            with zipfile.ZipFile('../data/STS_text.zip', 'r') as zip_ref:
-                zip_ref.extractall('../data/sts')
+        extract_sts_embeds(args, device, model, tokenizer)
 
-            wget.download("http://alt.qcri.org/semeval2017/task1/data/uploads/sts2017.gs.zip", '../data/STS_gt.zip')
-            with zipfile.ZipFile('../data/STS_gt.zip', 'r') as zip_ref:
-                zip_ref.extractall('../data/sts')
+    elif args.dataset == 'bucc':
+        extract_bucc_embeds(args, device, langs, model, tokenizer)
 
-            os.remove('../data/STS_gt.zip')
-            os.remove('../data/STS_text.zip')
 
-        def extract_sts_lng(file, track, sentences, lng_id):
-            rep = get_embeds(sentences, model, tokenizer, args, device)
-            torch.save(rep, f'../embs/{args.dataset}/{args.model}/{args.layer}/{track}/{lng_id}.pt')
-            if args.save_whitened:
-                whitened = torch.Tensor(whitening(rep.numpy()))
-                torch.save(whitened, f'../embs/{args.dataset}/{args.model}/{args.layer}/{track}/{lng_id}_whitened.pt')
-            if args.save_cbie:
-                n_cluster = max(rep.shape[0] // 300, 1)
-                cbie = torch.Tensor(
-                    cluster_based(rep.numpy(), n_cluster=n_cluster, n_pc=12, hidden_size=rep.shape[1]))
-                torch.save(cbie, f'../embs/{args.dataset}/{args.model}/{args.layer}/{track}/{lng_id}_cbie.pt')
+def download_tatoeba():
+    os.makedirs('../data/tatoeba')
+    # os.makedirs('../data/tatoeba-parallel')
+    os.makedirs('../data/labels/tatoeba')
+    wget.download("https://github.com/facebookresearch/LASER/archive/master.zip", "../data/")
+    with zipfile.ZipFile('../data/LASER-main.zip', 'r') as zip_ref:
+        zip_ref.extractall('../data/')
+    os.rename('../data/LASER-main/data/tatoeba/v1/', '../data/tatoeba-parallel/')
+    for sl3, sl2 in lang_dict_3_2.items():
+        if sl3 == 'eng' or sl3 == 'sun':
+            continue
+        src_file = f'../data/tatoeba-parallel/tatoeba.{sl3}-eng.{sl3}'
+        tgt_file = f'../data/tatoeba-parallel/tatoeba.{sl3}-eng.eng'
+        src_out = f'../data/tatoeba/{sl2}-en.{sl2}'
+        tgt_out = f'../data/tatoeba/{sl2}-en.en'
+        shutil.copy(src_file, src_out)
 
-        def extract_sts(file, track):
-            lng1_sent, lng2_sent = sentences_from_two_cols(f'../data/sts/{file}')
-            try:
-                os.makedirs(f'../embs/{args.dataset}/{args.model}/{args.layer}/{track}/')
-            except OSError:
-                print(f"embeddings for {track} already exist, skipping")
-                return
-            extract_sts_lng(file, track, lng1_sent, "lng1")
-            extract_sts_lng(file, track, lng2_sent, "lng2")
+        tgts = [line.strip() for line in open(tgt_file, 'r', encoding='utf-8')]
+        idx = range(len(tgts))
+        data = zip(tgts, idx)
+        labels_out = open(f'../data/labels/tatoeba/test-{sl2}.tsv', 'w', encoding='utf-8')
+        with open(tgt_out, 'w', encoding='utf-8') as ftgt:
+            for t, i in sorted(data, key=lambda x: x[0]):
+                ftgt.write(f'{t}\n')
+                labels_out.write(f'[{i}]\n')
+        labels_out.close()
+    os.remove('../data/LASER-main.zip')
+    shutil.rmtree('../data/LASER-main/')
 
-        print("Extract Track 2 ar-en.")
-        extract_sts('STS2017.eval.v1.1/STS.input.track2.ar-en.txt', 'track2-ar-en')
 
-        print("Extract Track 4a es-en.")
-        extract_sts('STS2017.eval.v1.1/STS.input.track4a.es-en.txt', 'track4a-es-en')
+def extract_tatoeba_embeds(args, device, langs, model, tokenizer):
+    if not os.path.isdir('../data/tatoeba'):
+        download_tatoeba()
+    for lang in langs:
+        print(f"Currently processing {lang}...")
+        try:
+            os.makedirs(f'../embs/{args.dataset}/{args.model}/{args.layer}/{lang}')
+        except OSError:
+            print(f"embeddings for {lang} already exist, skipping")
+            continue
+        if args.tatoeba_use_task_order:
+            tgt, eng = read_tatoeba_data_task(lang)
+        else:
+            tgt, eng = read_tatoeba_data(lang)
+        tgt_embeddings = get_embeds(tgt, model, tokenizer, args, device)
+        eng_embeddings = get_embeds(eng, model, tokenizer, args, device)
+        torch.save(tgt_embeddings, f'../embs/{args.dataset}/{args.model}/{args.layer}/{lang}/{lang}.pt')
+        torch.save(eng_embeddings, f'../embs/{args.dataset}/{args.model}/{args.layer}/{lang}/eng.pt')
+        if args.save_whitened:
+            tgt_whitened = torch.Tensor(whitening(tgt_embeddings.numpy()))
+            eng_whitened = torch.Tensor(whitening(eng_embeddings.numpy()))
+            torch.save(tgt_whitened, f'../embs/{args.dataset}/{args.model}/{args.layer}/{lang}/{lang}_whitened.pt')
+            torch.save(eng_whitened, f'../embs/{args.dataset}/{args.model}/{args.layer}/{lang}/eng_whitened.pt')
+        if args.save_cbie:
+            n_cluster = max(len(eng) // 300, 1)
+            tgt_cbie = torch.Tensor(cluster_based(
+                tgt_embeddings.numpy(), n_cluster=n_cluster, n_pc=12, hidden_size=tgt_embeddings.shape[1]))
+            eng_cbie = torch.Tensor(cluster_based(
+                eng_embeddings.numpy(), n_cluster=n_cluster, n_pc=12, hidden_size=eng_embeddings.shape[1]))
+            torch.save(tgt_cbie, f'../embs/{args.dataset}/{args.model}/{args.layer}/{lang}/{lang}_cbie.pt')
+            torch.save(eng_cbie, f'../embs/{args.dataset}/{args.model}/{args.layer}/{lang}/eng_cbie.pt')
+        print(f"Finished saving embeddings for {lang} in model {args.model}.")
 
-        print("Extract Track 4b es-en.")
-        extract_sts('STS2017.eval.v1.1/STS.input.track4b.es-en.txt', 'track4b-es-en')
 
-        print("Extract Track 6 tr-en.")
-        extract_sts('STS2017.eval.v1.1/STS.input.track6.tr-en.txt', 'track6-tr-en')
+def extract_sts_embeds(args, device, model, tokenizer):
+    if not os.path.exists('../data/sts'):
+        print("Download and extract the STS data.")
+        os.makedirs('../data/sts/')
+        wget.download("http://alt.qcri.org/semeval2017/task1/data/uploads/sts2017.eval.v1.1.zip",
+                      '../data/STS_text.zip')
+        with zipfile.ZipFile('../data/STS_text.zip', 'r') as zip_ref:
+            zip_ref.extractall('../data/sts')
 
-        print(f"Finished saving STS for model {args.model}.")
+        wget.download("http://alt.qcri.org/semeval2017/task1/data/uploads/sts2017.gs.zip", '../data/STS_gt.zip')
+        with zipfile.ZipFile('../data/STS_gt.zip', 'r') as zip_ref:
+            zip_ref.extractall('../data/sts')
+
+        os.remove('../data/STS_gt.zip')
+        os.remove('../data/STS_text.zip')
+
+    def extract_sts_lng(file, track, sentences, lng_id):
+        rep = get_embeds(sentences, model, tokenizer, args, device)
+        torch.save(rep, f'../embs/{args.dataset}/{args.model}/{args.layer}/{track}/{lng_id}.pt')
+        if args.save_whitened:
+            whitened = torch.Tensor(whitening(rep.numpy()))
+            torch.save(whitened, f'../embs/{args.dataset}/{args.model}/{args.layer}/{track}/{lng_id}_whitened.pt')
+        if args.save_cbie:
+            n_cluster = max(rep.shape[0] // 300, 1)
+            cbie = torch.Tensor(
+                cluster_based(rep.numpy(), n_cluster=n_cluster, n_pc=12, hidden_size=rep.shape[1]))
+            torch.save(cbie, f'../embs/{args.dataset}/{args.model}/{args.layer}/{track}/{lng_id}_cbie.pt')
+
+    def extract_sts(file, track):
+        lng1_sent, lng2_sent = sentences_from_two_cols(f'../data/sts/{file}')
+        try:
+            os.makedirs(f'../embs/{args.dataset}/{args.model}/{args.layer}/{track}/')
+        except OSError:
+            print(f"embeddings for {track} already exist, skipping")
+            return
+        extract_sts_lng(file, track, lng1_sent, "lng1")
+        extract_sts_lng(file, track, lng2_sent, "lng2")
+
+    print("Extract Track 2 ar-en.")
+    extract_sts('STS2017.eval.v1.1/STS.input.track2.ar-en.txt', 'track2-ar-en')
+    print("Extract Track 4a es-en.")
+    extract_sts('STS2017.eval.v1.1/STS.input.track4a.es-en.txt', 'track4a-es-en')
+    print("Extract Track 4b es-en.")
+    extract_sts('STS2017.eval.v1.1/STS.input.track4b.es-en.txt', 'track4b-es-en')
+    print("Extract Track 6 tr-en.")
+    extract_sts('STS2017.eval.v1.1/STS.input.track6.tr-en.txt', 'track6-tr-en')
+    print(f"Finished saving STS for model {args.model}.")
+
+
+def extract_wiki_embeds(args, device, langs, model, tokenizer):
+    # Loading Wikipedia datasets
+    df_su = pd.read_csv('../data/Wikipedia/Sundanese.csv', sep=',')
+    df_sw = pd.read_csv('../data/Wikipedia/Swahili.csv', sep=',')
+    df_en = pd.read_csv('../data/Wikipedia/English.csv', sep=',')
+    df_es = pd.read_csv('../data/Wikipedia/Spanish.csv', sep=',')
+    df_ar = pd.read_csv('../data/Wikipedia/Arabic.csv', sep=',')
+    df_tr = pd.read_csv('../data/Wikipedia/Turkish.csv', sep=',')
+    dfs = [df_ar, df_en, df_es, df_su, df_sw, df_tr]
+    for lang, df in zip(langs, dfs):
+        print(f"processing {lang}...")
+        try:
+            os.makedirs(f'../embs/{args.dataset}/{args.model}/{args.layer}/{lang}/')
+        except OSError:
+            print(f"embeddings for {lang} already exist, skipping")
+            continue
+        rep = get_embeds(df['Sentence'].tolist(), model, tokenizer, args, device)
+        torch.save(rep, f'../embs/{args.dataset}/{args.model}/{args.layer}/{lang}/{lang}.pt')
+        if args.save_whitened:
+            whitened = torch.Tensor(whitening(rep.numpy()))
+            torch.save(whitened, f'../embs/{args.dataset}/{args.model}/{args.layer}/{lang}/{lang}_whitened.pt')
+        if args.save_cbie:
+            n_cluster = max(rep.shape[0] // 300, 1)
+            cbie = torch.Tensor(
+                cluster_based(rep.numpy(), n_cluster=n_cluster, n_pc=12, hidden_size=rep.shape[1]))
+            torch.save(cbie, f'../embs/{args.dataset}/{args.model}/{args.layer}/{lang}/{lang}_cbie.pt')
+        print(f"Finished saving embeddings for {lang} in model {args.model}.")
 
 
 if __name__ == "__main__":
